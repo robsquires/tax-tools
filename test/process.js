9e31e70bb554b0153edfa6b7abee1cf8a54e85fd
Rob Squires <rob.squires@ft.com>
2019-10-24 00:50:17 +0100

    make api call to move money

--- test/process.js
+++ test/process.js
@@ -1,19 +1,24 @@
 
+require('dotenv').config()
 const sample = require('../sample.json');
 const processTransaction = require('../lib/process');
 
+const { ACC_ID, POT_ID } = process.env
 
-
-processTransaction(sample, [
-    {
-       criteria: { 
-            'data.counterparty.account_number': '123456',
-            'data.counterparty.sort_code': '010101',
-            'data.description': 'DIVIDEND'
-        },
-        action: {
-            potId: 123,
-            percentage: 0.3
+processTransaction(
+    sample,
+    [
+        {
+            criteria: { 
+                'data.counterparty.account_number': '123456',
+                'data.counterparty.sort_code': '010101',
+                'data.description': 'DIVIDEND'
+            },
+            action: {
+                potId: POT_ID,
+                percentage: 1.5
+            }
         }
-    }
-])
+    ],
+    ACC_ID,
+)
