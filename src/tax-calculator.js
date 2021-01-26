b23e12c4994dfde7f5a258c65e55fac7ce7be790
Rob Squires <rob.squires@ft.com>
2021-01-26 12:38:36 +0000

    implement function to apply tax

--- src/tax-calculator.js
+++ src/tax-calculator.js
@@ -3,16 +3,70 @@ class TaxCalculator {
         this.bands = bands
     }
 
-    totalTax(amount) {
-        const appliedBands = this.bands
-            // remove tax bands that are above the amount to tax
-            .filter(([lower]) => amount >= lower)
-            // calculate taxable amount applicable to each band
-            .map(([lower, upper, rate]) => {
-                return [amount > upper ? upper - lower : amount - lower, rate]
-            })
-        const totalTax = appliedBands.reduce((acc, [taxableAmount, rate]) => acc + taxableAmount * rate, 0)
+    calcNetPerBand(grossBalance, grossAmount, bandIdx = 0) {
+        if (!this.bands[bandIdx]) {
+            return
+        }
+
+        const [_, upper, rate] = this.bands[bandIdx]
+        const remaining = upper - grossBalance
+
+        let grossAllocated
+        if (grossBalance > upper) {
+            grossAllocated = 0
+        } else {
+            grossAllocated = grossAmount < remaining ? grossAmount : remaining
+        }
+
+        const netAmount = grossAllocated * rate
+        const amountFromNextBand = this.calcNetPerBand(
+            grossBalance + grossAllocated,
+            grossAmount - grossAllocated,
+            bandIdx + 1,
+        )
+        return amountFromNextBand ? [netAmount].concat(amountFromNextBand) : [netAmount]
+    }
+
+    calcGrossPerBand(grossBalance, netAmount, bandIdx) {
+        if (!this.bands[bandIdx]) {
+            return
+        }
+
+        const [_, upper, rate] = this.bands[bandIdx]
+        const grossAmount = netAmount / (1 - rate)
+        const grossRemaining = upper - grossBalance
+
+        let grossAllocated
+        if (grossBalance > upper) {
+            grossAllocated = 0
+        } else {
+            grossAllocated = grossAmount > grossRemaining ? grossRemaining : grossAmount
+        }
+
+        const netAllocated = grossAllocated * (1 - rate)
+
+        const amountFromNextBand = this.calcGrossPerBand(
+            grossBalance + grossAllocated,
+            netAmount - netAllocated,
+            bandIdx + 1,
+        )
+        return amountFromNextBand ? [grossAllocated].concat(amountFromNextBand) : [grossAllocated]
+    }
+
+    calculateNet(grossBalance, grossAmount) {
+        const netPerBand = this.calcNetPerBand(grossBalance, grossAmount, 0)
+        const totalTax = netPerBand.reduce((acc, netAmount) => acc + netAmount, 0)
+        console.log(netPerBand, totalTax)
+
         return parseFloat(totalTax.toFixed(2))
+    }
+
+    grossUp(grossBalance, netAmount) {
+        const grossPerBand = this.calcGrossPerBand(grossBalance, netAmount, 0)
+        const totalGross = grossPerBand.reduce((acc, grossAmount) => acc + grossAmount, 0)
+        console.log(grossPerBand, totalGross)
+
+        return parseFloat(totalGross.toFixed(2))
     }
 }
 
