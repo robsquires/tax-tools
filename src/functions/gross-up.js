203fc88771b3ed79827fa74c41054006655370b1
Rob Squires <rob.squires@ft.com>
2021-02-05 11:47:39 +0000

    return gross amount in api body
    Easier to process in Apple shortcut

--- src/functions/gross-up.js
+++ src/functions/gross-up.js
@@ -5,16 +5,6 @@ module.exports.handler = async (event) => {
   const netAmount = parseFloat(queryStringParameters.amount)
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        grossAmount: await tax.calculateGrossPayment(netAmount),
-        netAmount,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+    body: await tax.calculateGrossPayment(netAmount),
+  }
+}
