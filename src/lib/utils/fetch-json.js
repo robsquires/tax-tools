fc9ae29d4290b322f09da35eae626c7669bbd60d
Rob Squires <rob.squires@ft.com>
2021-02-08 21:59:10 +0000

    handle fetch errors and responses

---
+++ src/lib/utils/fetch-json.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch')
+const createError = require('http-errors')
+
+/**
+ * Useful reference for fetch errors
+ * https://github.com/node-fetch/node-fetch/blob/master/docs/ERROR-HANDLING.md
+ */
+
+async function fetchJson (...args) {
+    // network, DNS other operational errors thrown here
+    const response = await fetch(...args)
+    const bodyText = await response.text()
+    let body
+    try {
+        // non-json responses common when server unavailable
+        body = JSON.parse(bodyText)
+    } catch (err) {
+        // catch the body can be logged
+        body = bodyText
+    }
+
+    if (!response.ok) {
+        // log body in-case contains useful info
+        console.log(body)
+        // convert to http-error
+        throw createError(response.status)
+    }
+
+    return body
+}
+
+module.exports = fetchJson
---
+++ src/lib/utils/fetch-json.test.js
@@ -0,0 +1,36 @@
+jest.mock('node-fetch', () => require('fetch-mock').sandbox())
+
+const fetchJson = require('./fetch-json')
+const fetchMock = require('node-fetch')
+
+const url = 'http://localhost/index.json'
+const options = {
+    method: 'GET',
+    headers: { 'content-type': 'application/json' }
+}
+
+describe('fetch-json', () => {
+
+    afterEach(() => fetchMock.reset())
+
+    test('200 response', async () => {
+        fetchMock.mock(url, { status: 200, body: '{ "data": {} }' } , options)
+        expect(await fetchJson(url, options)).toEqual({ data: {} })
+    })
+
+    test('4xx response', async () => {
+        fetchMock.mock(url, { status: 400, body: '{ "data": {} }' } , options)
+        await expect(fetchJson(url, options)).rejects.toEqual(new Error('Bad Request'))
+    })
+
+    test('503 response', async () => {
+        fetchMock.mock(url, { status: 503, body: 'There was an error' } , options)
+        await expect(fetchJson(url, options)).rejects.toEqual(new Error('Service Unavailable'))
+    })
+
+    test('networking error', async () => {
+        const fetchError = new Error('ECONNRESET')
+        fetchMock.mock(url, { throws: fetchError } , options)
+        await expect(fetchJson(url, options)).rejects.toEqual(fetchError)
+    })
+})
