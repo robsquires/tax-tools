4a15fbe53ae0de6b2cde85790f2455e006c58fd5
Rob Squires <rob.squires@ft.com>
2021-02-10 09:33:02 +0000

    configure linting

--- test/process.js
+++ test/process.js
@@ -1,7 +1,6 @@
-
 require('dotenv').config()
-const sample = require('../sample.json');
-const processTransaction = require('../lib/process');
+const sample = require('../sample.json')
+const processTransaction = require('../lib/process')
 
 const { ACC_ID, POT_ID } = process.env
 
@@ -9,16 +8,16 @@ processTransaction(
     sample,
     [
         {
-            criteria: { 
+            criteria: {
                 'data.counterparty.account_number': '123456',
                 'data.counterparty.sort_code': '010101',
-                'data.description': 'DIVIDEND'
+                'data.description': 'DIVIDEND',
             },
             action: {
                 potId: POT_ID,
-                percentage: 1.5
-            }
-        }
+                percentage: 1.5,
+            },
+        },
     ],
     ACC_ID,
 )
