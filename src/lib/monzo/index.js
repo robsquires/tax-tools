b50cd21352f7bcbbb90f733c0bb18552ba5afa86
Rob Squires <rob.squires@ft.com>
2021-02-12 18:22:10 +0000

    spin up process payment function

--- src/lib/monzo/index.js
+++ src/lib/monzo/index.js
@@ -1,1 +1,15 @@
-module.exports = {}
+const { tax, taxYear } = require('../tax')
+const { dataBucket } = require('../aws')
+const Auth = require('../auth')
+const HttpClient = require('./http')
+const MonzoClient = require('./client')
+const { TransactionLog } = require('./transaction-log')
+const ProcessPayment = require('./process-payment')
+
+const transactionLog = new TransactionLog(taxYear, dataBucket)
+const http = new HttpClient('https://api.monzo.com', Auth)
+const monzoClient = new MonzoClient(http)
+
+module.exports = {
+    processPayment: ProcessPayment(tax, transactionLog, monzoClient),
+}
