a17ab42b1faac9cf3498344e1e313cd770f9662f
Rob Squires <rob.squires@ft.com>
2021-02-16 13:01:52 +0000

    pass transaction id as cli param

--- scripts/process-payment.js
+++ scripts/process-payment.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const { POT_ID, ACC_ID } = process.env
 const Monzo = require('../src/lib/monzo')
 
-const txId = 'tx_0000A4Ih1lMs2bSFFVAsJF'
+const txId = process.argv[2]
 
 Monzo.processPayment(txId, POT_ID, ACC_ID).then(console.log).catch(console.error)
