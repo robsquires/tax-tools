d8076503d355c86f4b39ed30d415bf105aae53e1
Rob Squires <rob.squires@ft.com>
2021-02-17 17:14:21 +0000

    add debugging to s3 bucket

--- src/lib/aws/s3-bucket.js
+++ src/lib/aws/s3-bucket.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('s3-bucket')
 class S3Bucket {
     constructor(s3, bucket) {
         this.s3 = s3
@@ -5,6 +6,7 @@ class S3Bucket {
     }
 
     read(objectKey) {
+        debug('read', this.bucket, objectKey)
         return new Promise((resolve, reject) => {
             this.s3.getObject({ Bucket: this.bucket, Key: objectKey }, (err, data) =>
                 err ? reject(err) : resolve(JSON.parse(data.Body)),
@@ -13,6 +15,7 @@ class S3Bucket {
     }
 
     write(objectKey, data) {
+        debug('write', this.bucket, objectKey)
         return new Promise((resolve, reject) => {
             this.s3.putObject({ Bucket: this.bucket, Key: objectKey, Body: JSON.stringify(data) }, (err, data) =>
                 err ? reject(err) : resolve(data),
