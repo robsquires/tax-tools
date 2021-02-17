0ccb62255a1736bfc31954faa27b0d8502725b09
Rob Squires <rob.squires@ft.com>
2021-02-17 10:59:48 +0000

    validate gross-up http params

--- src/functions/gross-up.js
+++ src/functions/gross-up.js
@@ -1,7 +1,18 @@
 const { tax } = require('../lib/tax')
 const Money = require('../lib/utils/money')
+const Joi = require('joi')
+
+const schema = Joi.object({
+    amount: Joi.number().required(),
+})
+
 module.exports.handler = async (event) => {
     const { queryStringParameters } = event
+    const { error } = schema.validate(queryStringParameters || {})
+    if (error) {
+        return { statusCode: 400, body: error.message }
+    }
+
     const netAmount = Money.fromPounds(queryStringParameters.amount)
     return {
         statusCode: 200,
